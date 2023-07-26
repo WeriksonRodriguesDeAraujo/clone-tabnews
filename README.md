36413c426770c25d42398480bc19113d9aa460bb
Wérikson Rodrigues de Araujo <67488006+WeriksonRodriguesDeAraujo@users.noreply.github.com>
2023-07-26 22:27:31 +0000

    Adicionando scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 projeto didático se baseando no https://www.tabnews.com.br no curso https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Booleans LTDA</h1>
+  return <h1>Booleans LTDA</h1>;
 }
 
 export default Home;
