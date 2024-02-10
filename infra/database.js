6bc802cdcc34bb5a6d5adae91025b1de34613d95
Wérikson Rodrigues de Araujo <67488006+WeriksonRodriguesDeAraujo@users.noreply.github.com>
2024-02-10 11:29:08 +0000

    Fazendo o `client.connect()` do `database.js` mais robusto a erros

--- infra/database.js
+++ infra/database.js
@@ -8,13 +8,23 @@ async function query(objectQuery) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD
   });
-  client.connect();
 
+  console.log("variáveis conexão com o postgres", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD
+  });
+  
   try {   
+    client.connect();
+
     const result = await client.query(objectQuery);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
