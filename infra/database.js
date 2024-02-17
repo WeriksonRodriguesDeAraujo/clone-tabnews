e2c3cf52e4997a6643669b910f0c41270ddacdec
Wérikson Rodrigues de Araujo <67488006+WeriksonRodriguesDeAraujo@users.noreply.github.com>
2024-02-17 22:25:27 +0000

    Adicionando método mais robusto de configuração SSL

--- infra/database.js
+++ infra/database.js
@@ -7,15 +7,7 @@ async function query(objectQuery) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === 'development' ? false : true
-  });
-
-  console.log("variáveis conexão com o postgres", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD
+    ssl: getSSLValues()
   });
   
   try {   
@@ -29,6 +21,16 @@ async function query(objectQuery) {
   } finally {
     await client.end();
   }
+}
+
+function getSSLValues() {
+  if(process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA
+    }
+  }
+
+  return process.env.NODE_ENV === 'development' ? false : true;
 }
 
 export default {
