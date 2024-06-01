9d2f52fc364fc75d7955da7d8507a90f72f78c96
Wérikson Rodrigues de Araujo <67488006+WeriksonRodriguesDeAraujo@users.noreply.github.com>
2024-06-01 14:02:23 +0000

    Adicionando script `wait-for-postgres.js` para esperar conexão com o banco de dados

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,18 @@
+const { exec } = require("node:child_process");
+
+function checkPostgres() {
+  exec('docker exec postgres-dev pg_isready --host localhost', handleReturn);
+
+  function handleReturn(error, stdout) {
+    if(stdout.search("accepting connections") === -1) {
+      console.log("Não está aceitando conexões ainda.");
+      setTimeout(() => checkPostgres(), 3000);
+      return;
+    }
+    
+    console.log("🟢 Postgres está pronto e está aceitando conexões.");
+  }
+};
+
+console.log("🔴 Aguardando o postgres aceitar conexões.");
+checkPostgres();
