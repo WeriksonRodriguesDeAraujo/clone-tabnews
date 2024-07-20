e888c76f78ccc85a779eddc18f6ad3c8d43a80f9
Wérikson Rodrigues de Araujo <67488006+WeriksonRodriguesDeAraujo@users.noreply.github.com>
2024-07-20 12:39:20 +0000

    Corrigindo estilo dos arquivos com o `lint:fix` usando prettier

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -1,18 +1,18 @@
 const { exec } = require("node:child_process");
 
 function checkPostgres() {
-  exec('docker exec postgres-dev pg_isready --host localhost', handleReturn);
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
 
   function handleReturn(error, stdout) {
-    if(stdout.search("accepting connections") === -1) {
+    if (stdout.search("accepting connections") === -1) {
       console.log("Não está aceitando conexões ainda.");
       setTimeout(() => checkPostgres(), 3000);
       return;
     }
-    
+
     console.log("🟢 Postgres está pronto e está aceitando conexões.\n");
   }
-};
+}
 
 console.log("🔴 Aguardando o postgres aceitar conexões.");
 checkPostgres();
