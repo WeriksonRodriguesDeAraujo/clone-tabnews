07f3ec8f8083175492ff4171bbc1dc300a7b1013
Wérikson Rodrigues de Araujo <67488006+WeriksonRodriguesDeAraujo@users.noreply.github.com>
2025-01-11 23:34:56 +0000

    feat: cria e usa InternalServerError em /api/vl/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,37 +1,47 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/errors.js";
 
 async function status(request, response) {
-  const updatedAt = new Date().toISOString();
-  const resultMaxConections = await database.query(`SHOW max_connections;`);
-  const maxConectionsValue = parseInt(
-    resultMaxConections.rows[0].max_connections,
-  );
+  try {
+    const updatedAt = new Date().toISOString();
+    const resultMaxConections = await database.query(`SHOW max_connections;`);
+    const maxConectionsValue = parseInt(
+      resultMaxConections.rows[0].max_connections,
+    );
 
-  const databaseName = process.env.POSTGRES_DB;
-  const resultActiveConections = await database.query({
-    text: `SELECT
-    COUNT(*)::int
-  FROM
-    pg_stat_activity
-  WHERE
-    datname = $1;`,
-    values: [databaseName],
-  });
-  const activeConectionsValue = resultActiveConections.rows[0].count;
+    const databaseName = process.env.POSTGRES_DB;
+    const resultActiveConections = await database.query({
+      text: `SELECT
+      COUNT(*)::int
+    FROM
+      pg_stat_activity
+    WHERE
+      datname = $1;`,
+      values: [databaseName],
+    });
+    const activeConectionsValue = resultActiveConections.rows[0].count;
 
-  const resultVersion = await database.query(`SHOW server_version;`);
-  const versionValue = resultVersion.rows[0].server_version;
+    const resultVersion = await database.query(`SHOW server_version;`);
+    const versionValue = resultVersion.rows[0].server_version;
 
-  response.status(200).json({
-    updated_at: updatedAt,
-    database: {
-      dependencies: {
-        version: versionValue,
-        max_conections: maxConectionsValue,
-        active_conections: activeConectionsValue,
+    response.status(200).json({
+      updated_at: updatedAt,
+      database: {
+        dependencies: {
+          version: versionValue,
+          max_conections: maxConectionsValue,
+          active_conections: activeConectionsValue,
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
+
+    console.error(publicErrorObject);
+    response.status(500).json(publicErrorObject);
+  }
 }
 
 export default status;
