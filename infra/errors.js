a37602370fa9a81e17d343ad54ec508981bdc0af
Wérikson Rodrigues de Araujo <67488006+WeriksonRodriguesDeAraujo@users.noreply.github.com>
2025-01-25 22:32:55 +0000

    feat: adiciona `MethodNotAllowedError` e `ServiceError`

--- infra/errors.js
+++ infra/errors.js
@@ -18,3 +18,43 @@ export class InternalServerError extends Error {
     };
   }
 }
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Método não permitido para esse endpoint.");
+
+    this.name = "MethodNotAllowedError";
+    this.action = "Verifique se esse método HTTP é valido para esse endpoint.";
+    this.statusCode = 405;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ cause, message }) {
+    super(message || "Serviço indisponível no momento.", {
+      cause,
+    });
+
+    this.name = "ServiceError";
+    this.action = "Verifique se o serviço está disponível.";
+    this.statusCode = 503;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
