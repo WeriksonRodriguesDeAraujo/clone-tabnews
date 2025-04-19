2d1804af7b3a0f0b9e0b807e6d142f1c739cd9be
Wérikson Rodrigues de Araujo <67488006+WeriksonRodriguesDeAraujo@users.noreply.github.com>
2025-04-19 22:23:14 +0000

    feat: cria modelo `user`  e o endpoint  `/api/v1/users`

---
+++ models/user.js
@@ -0,0 +1,81 @@
+import database from "infra/database.js";
+import { ValidationError } from "infra/errors.js";
+
+async function create(request) {
+  async function runInsertQuery(request) {
+    const query = `
+INSERT INTO 
+  users (username, email, password)
+VALUES 
+  ($1, $2, $3) 
+RETURNING *
+;`;
+
+    const result = await database.query({
+      text: query,
+      values: [request.username, request.email, request.password]
+    })
+
+    return result.rows[0];
+  };
+
+  // TODO Pode ser feito uma única validação para evitar consultas excessivas ao banco.
+  await _validateUniqueEmail(request.email);
+  await _validateUniqueUsername(request.username);
+
+  const newUser = await runInsertQuery(request);
+  return newUser;
+};
+
+async function _validateUniqueEmail(email) {
+  const query = `
+SELECT 
+  email,
+  username
+FROM
+  users
+WHERE
+  LOWER(email) = LOWER($1)
+;`;
+
+  const result = await database.query({
+    text: query,
+    values: [email]
+  })
+
+  if (result.rowCount > 0) {
+    throw new ValidationError({
+      message: "O email informado já está sendo utilizado.",
+      action: "Utilize outro email para realizar o cadastro."
+    })
+  }
+}
+
+async function _validateUniqueUsername(username) {
+  const query = `
+SELECT 
+  username
+FROM
+  users
+WHERE
+  LOWER(username) = LOWER($1)
+;`;
+
+  const result = await database.query({
+    text: query,
+    values: [username]
+  })
+
+  if (result.rowCount > 0) {
+    throw new ValidationError({
+      message: "O nome do usuário informado já está sendo utilizado.",
+      action: "Utilize outro nome para realizar o cadastro."
+    })
+  }
+}
+
+const user = {
+  create
+};
+
+export default user;
