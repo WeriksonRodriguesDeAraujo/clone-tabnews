4ec4f93778d6a76527803dbb0a38f0b374ecfc44
Wérikson Rodrigues de Araujo <67488006+WeriksonRodriguesDeAraujo@users.noreply.github.com>
2025-09-21 14:29:08 +0000

    feat: implementa expiração de sessão

--- models/session.js
+++ models/session.js
@@ -75,10 +75,30 @@ async function renew(sessionId) {
   return results.rows[0];
 }
 
+async function expireById(sessionId) {
+  const results = await database.query({
+    text: `
+      UPDATE
+        sessions
+      SET
+        expires_at = expires_at - interval '1 year',
+        updated_at = NOW()
+      WHERE
+        id = $1
+      RETURNING
+        *
+    ;`,
+    values: [sessionId],
+  });
+
+  return results.rows[0];
+}
+
 const session = {
   create,
   findOneValidByToken,
   renew,
+  expireById,
   EXPIRATION_IN_MILLISECONDS,
 };
 
